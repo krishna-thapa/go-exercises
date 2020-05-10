d0874e7ee4c8a57e13529367283fc129fd36577b
krishna <krishna.nature91@gmail.com>
2020-05-10 14:22:02 +0100

    Added mutation

---
+++ src/go-graphql/crud/README.md
@@ -0,0 +1,15 @@
+# Go GraphQL CRUD example
+
+## Implement create, read, update and delete on Go.
+
+## See `request.http` file to run and see the url for CRUD operation
+
+## Instal Visual studio code plugin to run REST client: https://marketplace.visualstudio.com/items?itemName=humao.rest-client
+
+
+## Future work
+- Implement SQLLite for Demo
+- Implement ORM 
+- Implement Redis 
+- Find to send query in response body instead in url
+- Implement Docker 
--- src/go-graphql/crud/main.go
+++ src/go-graphql/crud/main.go
@@ -2,32 +2,36 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
 	"fmt"
-	"github.com/graphql-go/graphql"
+	"math/rand"
+	"net/http"
+	"reflect"
 	"time"
+
+	"github.com/graphql-go/graphql"
 )
 
-// class for one quote 
+// class for one quote
 type Quote struct {
-	ID		int64		`json:"id"`
-	Quote	string		`json:"quote"`
-	Author	string		`json:"author,omitempty"`
-	Tags	[]string	`json:"tags,omitempty"`
-	Date	time.Time	`json:"date"`
+	ID     int64     `json:"id"`
+	Quote  string    `json:"quote"`
+	Author string    `json:"author,omitempty"`
+	Tags   []string  `json:"tags,omitempty"`
+	Date   time.Time `json:"date"`
 }
+
 // https://www.sohamkamani.com/golang/2018-07-19-golang-omitempty/
 
-var quotes = []Quote {
+var quotes = []Quote{
 	{
-		ID:		1,
-		Quote:	"Hello world",
-		Author:	"John Will",
-		Tags:	[]string{
-			"love", 
+		ID:     1,
+		Quote:  "Hello world",
+		Author: "John Will",
+		Tags: []string{
+			"love",
 			"hate",
 		},
-		Date:	time.Now(),  //change to default date
+		Date: time.Now(), //change to default date
 	},
 }
 
@@ -63,7 +67,7 @@ var queryType = graphql.NewObject(
 			   http://localhost:8080/quote?query={quote(id:1){quote,author,tags,date}}
 			*/
 			"quote": &graphql.Field{
-				Type: quoteType,
+				Type:        quoteType,
 				Description: "Get quote by id",
 				Args: graphql.FieldConfigArgument{
 					"id": &graphql.ArgumentConfig{
@@ -74,7 +78,7 @@ var queryType = graphql.NewObject(
 					id, ok := p.Args["id"].(int)
 					if ok {
 						// find quote
-						for _, quote := range quotes{
+						for _, quote := range quotes {
 							if int(quote.ID) == id {
 								return quote, nil
 							}
@@ -88,7 +92,7 @@ var queryType = graphql.NewObject(
 			   http://localhost:8080/quotet?query={list{id,quote,author,tags,date}}
 			*/
 			"list": &graphql.Field{
-				Type: graphql.NewList(quoteType),
+				Type:        graphql.NewList(quoteType),
 				Description: "Get all the quotes",
 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 					return quotes, nil
@@ -98,15 +102,59 @@ var queryType = graphql.NewObject(
 	},
 )
 
+var mutationType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+
+		// Create a new quote
+		"create": &graphql.Field{
+			Type:        quoteType,
+			Description: "Create a new quote",
+			Args: graphql.FieldConfigArgument{
+				"quote": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"author": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"tags": &graphql.ArgumentConfig{
+					Type: graphql.NewList(graphql.String),
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				//koverflow.com/questions/44027826/convert-interface-to-string-in-golang
+				tags := p.Args["tags"].([]interface{})
+				fmt.Println("Type of tags: ",reflect.TypeOf(tags))
+				stringTags := make([]string, len(tags))
+				for i, v := range tags {
+					stringTags[i] = fmt.Sprint(v)
+				}
+				//https://flaviocopes.com/go-random/
+				rand.Seed(time.Now().UnixNano())
+				quote := Quote{
+					ID:     int64(rand.Intn(100000)), // generate random ID
+					Quote:  p.Args["quote"].(string),
+					Author: p.Args["author"].(string),
+					Tags:   stringTags,
+					Date:   time.Now(),
+				}
+				quotes = append(quotes, quote)
+				return quote, nil
+			},
+		},
+	},
+})
 var schema, _ = graphql.NewSchema(
 	graphql.SchemaConfig{
-		Query: queryType,
+		Query:    queryType,
+		Mutation: mutationType,
 	},
 )
 
 func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	fmt.Printf(query)
 	result := graphql.Do(graphql.Params{
-		Schema: schema,
+		Schema:        schema,
 		RequestString: query,
 	})
 	if len(result.Errors) > 0 {
@@ -115,7 +163,7 @@ func executeQuery(query string, schema graphql.Schema) *graphql.Result {
 	return result
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/quote", func(w http.ResponseWriter, r *http.Request) {
 		result := executeQuery(r.URL.Query().Get("query"), schema)
 		json.NewEncoder(w).Encode(result)
